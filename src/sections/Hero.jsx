7e53e198da099fee43406a4fb75636dc3a9e0b2a
lkhyal <lkhyal.ab@gmail.com>
2025-10-29 23:14:57 +0100

    adding the type effect

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,14 +1,59 @@
 import { ArrowDown } from "lucide-react";
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+const words = [
+  "Bammi Abdelali",
+  "A Frontend Developer",
+  "A React Enthusiast",
+  "UI Designer",
+];
 const Hero = () => {
+  const [currentWord, setCurrentWord] = useState("");
+
+  useEffect(() => {
+    let duration = 300;
+    let idx = 0;
+    let word = words[idx];
+    let isDeleting = false;
+    let text = "";
+    let wordIdx = 0;
+    let timer;
+    function type() {
+      if (isDeleting) {
+        duration = 100;
+        text = text.slice(0, text.length - 1);
+      } else {
+        duration = 300;
+        text = word.slice(0, text.length + 1);
+      }
+      if (isDeleting) {
+        if (text === "") {
+          isDeleting = false;
+          idx = idx == words.length - 1 ? 0 : idx + 1;
+          word = words[idx];
+          duration = 300;
+        }
+      } else {
+        if (text === word) {
+          isDeleting = true;
+          duration = 1000;
+        }
+      }
+
+      setCurrentWord(text);
+      timer = setTimeout(type, duration);
+    }
+    // const timerId = setTimeout(type, duration);
+    type();
+
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <section
       id="home"
       className="relative w-full h-screen pb-5 p-10 flex flex-col items-center justify-center  gap-10"
     >
       <h1 className="sectionTitle text-secondary text-center">
-        Hey, I'm <span className="text-primary">Abdelali</span> Baami
+        Hey, I'm <span className="text-primary">{currentWord}</span>
       </h1>
       <p className="text-accent md:w-1/2 text-center font-medium leading-8">
         Front-end developer crafting fast, accessible, and pixel-perfect web
